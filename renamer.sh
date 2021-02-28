ee419bdc7dad121d688d0b298a337344d5f37ee6
Scully <scully@fluxfederation.com>
2021-02-28 16:22:56 +1300

    yay, working file renamer

--- renamer.sh
+++ renamer.sh
@@ -1,63 +1,55 @@
 #!/bin/bash
-
-#set -x
-
-# data_source="/Users/$USER/Desktop/testdata/"
-# echo $data_source
-
-# prefix='Wellington_Touch_Association_Incorporated_-_'
-# echo $prefix
-
-# rm -rfv "/Users/$USER/Desktop/testdataoutput/" && mkdir -p "/Users/$USER/Desktop/testdataoutput/"
-
-# find "/Users/$USER/Desktop/testdata" -name "*$prefix*" -print0 |
- 
-
-# find ../../Downloads -name "*$PREFIX*" -exec sh -c 'x="{}"; cp "$x" "${x}_digs"' \;
-
 # set -x
 
-base_dir=/Users/scully/Desktop/testdata
-# echo 'base_dir'
-# echo $base_dir
-# echo
-
 # Note: $1 should look like "2021-01"
+
+# CREATE new dir for copied files
 new_dir="/Users/scully/Desktop/$1_-_Monthly_Financial_Board_Reports"
 mkdir -p $(echo $new_dir)
 rm -f $(echo $new_dir)/*
 
-echo 'new_dir'
-echo $new_dir
-echo
+# echo 'new_dir'
+# echo $new_dir
+# echo
 
+# NAME change/replacement
 start=Wellington_Touch_Association_Incorporated_
 rplc="$1_WTA_Board_Meeting_"
-echo 'rplc'
-echo $rplc
-echo
+# echo 'rplc'
+# echo $rplc
+# echo
+
+# WHERE to find the files we want to rename/copy
+base_dir=/Users/scully/Downloads
+# echo 'base_dir'
+# echo $base_dir
+# echo
 
 for i in $(find $base_dir -name "$start*"); do
 	# echo
 	# echo '$i'
   # echo $i;
+
+	# TITLE change the title of the file
   first=${i/$start/$rplc}
+	
 	# echo
 	# echo '$first'
   # echo $first
+
+	# STRIP nonsense off the end
   next=$(echo "$first" | sed "s/__WTA[0-9]*_//g")
+	
 	echo
-	echo '$next1'
+	echo '$next'
 	echo $next
 
-	# line=$(echo "$next" | sed "s/_WTA[0-9]*_//g")
-	# echo
-	# echo '$line'
-	# echo $line
-	# next1=${next/WTA[0-9]*/.pdf}
-	# next2=${next1/ .pdf/.pdf}
-	# echo '$next2'
-  # echo $next2
-  # cp "$i" "$more"
+	# PUT in a new folder
+	new_location=$(echo "$next" | sed "s~$base_dir~$new_dir~g")
+	echo
+	echo '$new_location'
+	echo $new_location
+
+  cp "$i" "$new_location"
 	# exit 0
 done
